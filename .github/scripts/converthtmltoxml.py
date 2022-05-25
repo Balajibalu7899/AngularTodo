545400bc2986fb9f55c3a37740b79d33a6a89ee3
Balaji GV <72762178+Balajibalu7899@users.noreply.github.com>
2022-05-25 16:14:58 +0530

    Update converthtmltoxml.py

--- .github/scripts/converthtmltoxml.py
+++ .github/scripts/converthtmltoxml.py
@@ -1,1 +1,8 @@
 
+import  jpype     
+import  asposecells     
+jpype.startJVM() 
+from asposecells.api import Workbook
+workbook = Workbook("input.html");
+workbook.Save("Output.xml");
+jpype.shutdownJVM()
